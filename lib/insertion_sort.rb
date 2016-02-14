7464cba00ca4c403b66600a7b987bf8a2d2f9cdb
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-02-14 14:48:36 -0700

    second part of insertion working, working on first

--- lib/insertion_sort.rb
+++ lib/insertion_sort.rb
@@ -1,15 +1,32 @@
+require 'pry'
 class InsertionSort
 
   def sort(num_array)
-    sorted = []
-    num = 0
-    until num_array[num].nil?
-      if num_array[num] < num_array[num + 1]
-        sorted << num_array[num]
-      else num_array[num] > num_array[num + 1]
-        num_array.insert(num_array[num + 1], )
+    i = index = num_array.length - 1
+    
+      while i > 0 && num_array[i] < num_array[i - 1]
+        num_array[i-1], num_array[i] = num_array[i], num_array[i-1]
+        i -= 1
       end
-    end
+      num_array
   end
+
+
+
+
+
+
+
+
+
+  # def sort(arr)
+  #   return arr if arr.size<=1
+  #   i=arr.size-1
+  #   while arr[i-1]>arr[i] and i>0
+  #     arr[i],arr[i-1] = arr[i-1],arr[i]
+  #     i-=1
+  #   end
+  #   arr
+  # end
 
 end
--- test/insertion_sort_test.rb
+++ test/insertion_sort_test.rb
@@ -2,9 +2,15 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/insertion_sort.rb'
 
-# class InsertionSortTest < Minitest::Test
-#   def test_inserting_once
-#     sorter = InsertionSort.new
-#     assert_equal [2,3,1], sorter.sort([2,3,1])
-#   end
-# end
+class InsertionSortTest < Minitest::Test
+  def test_inserting_once
+    sorter = InsertionSort.new
+    assert_equal [1,2], sorter.sort([2,1])
+  end
+
+  def test_insertion_twice
+    sorter = InsertionSort.new
+    assert_equal [1,2,3], sorter.sort([2,1,3])
+  end
+
+end
