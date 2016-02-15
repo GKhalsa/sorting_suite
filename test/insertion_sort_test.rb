bd92f4b9669cb3989951a64c2144c0b8ab54e2d5
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-02-14 17:43:42 -0700

    added more tests

--- test/insertion_sort_test.rb
+++ test/insertion_sort_test.rb
@@ -18,8 +18,33 @@ class InsertionSortTest < Minitest::Test
     assert_equal [1,2,3,4], sorter.sort([2,3,1,4])
   end
 
-  def test_insertion_thrice
+  def test_inserting_four_times
     sorter = InsertionSort.new
     assert_equal [1,2,3,4,5], sorter.sort([5,4,3,2,1])
   end
+  def test_insertion_with_empty_array
+    sorter = InsertionSort.new
+    assert_equal [], sorter.sort([])
+  end
+
+  def test_insertion_with_letters
+    sorter = InsertionSort.new
+    assert_equal ["a","b","c","d"], sorter.sort(["d","c","a","b"])
+  end
+
+  def test_insertion_with_letters_and_symbols
+    sorter = InsertionSort.new
+    assert_equal ["%","*","a","b","c","d"], sorter.sort(["*","d","c","a","b","%"])
+  end
+
+  def test_insertion_with_sorted_array
+    sorter = InsertionSort.new
+    assert_equal [1,2,3,4,5], sorter.sort([1,2,3,4,5])
+  end
+
+  def test_insertion_with_one_char
+    sorter = InsertionSort.new
+    assert_equal [1], sorter.sort([1])
+  end
+
 end
