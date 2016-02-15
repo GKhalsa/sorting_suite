f5f490b5d94994f7d33f38010a732ab4b195210b
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-02-14 21:53:54 -0700

    index tracker for insertion

--- lib/insertion_sort.rb
+++ lib/insertion_sort.rb
@@ -2,13 +2,15 @@ require 'pry'
 class InsertionSort
 
   def sort(num_array)
+    x = 1
     (num_array.length - 1).times do
-      i = 1
+      i = x
+      x += 1
       while i > 0
         if num_array[i] < num_array[i - 1]
           num_array[i - 1], num_array[i] = num_array[i], num_array[i - 1]
           i -= 1
-        elsif
+        else
           i +=  1
         end
         break if i > num_array.count - 1
