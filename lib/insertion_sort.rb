f9b236c9fdda982e57acdaeb4f51e6cb5649192d
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-02-15 15:16:24 -0700

    refactored insertion sort

--- lib/insertion_sort.rb
+++ lib/insertion_sort.rb
@@ -1,19 +1,14 @@
-require 'pry'
 class InsertionSort
 
   def sort(num_array)
-    x = 1
-    (num_array.length - 1).times do
-      i = x
-      x += 1
-      while i > 0
+    (num_array.length).times do |i|
+      while i > 0 && i < num_array.count
         if num_array[i] < num_array[i - 1]
           num_array[i - 1], num_array[i] = num_array[i], num_array[i - 1]
           i -= 1
         else
           i +=  1
         end
-        break if i > num_array.count - 1
       end
     end
     num_array
